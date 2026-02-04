b50310c2bc22b65d3acdca85e31658ff1376feec
John Christian Alvarez <johnchristianalvarezbsit2k19@gmail.com>
2026-02-04 17:04:37 +0800

    fixed the button on page 1

--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -4,7 +4,7 @@ export default function Page1() {
   return (
     <div>
       <h1>Page 1</h1>
-      <button onClick={() => alert("Yehey You CLiked Me")}>Click Me</button>
+      <button onClick={() => alert("Yehey Im Fixed")}>Click Me</button>
     </div>
   );
 }
